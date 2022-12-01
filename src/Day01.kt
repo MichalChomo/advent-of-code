0b6cf0ad8c096516a4ea7a6735ee4ef3ffb8141f
Michal Chomo <chomo.michal.sk@gmail.com>
2022-12-01 13:20:42 +0100

    add day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getListOfSummedElfCalories(input: String) =
+        input.split("\n\n")
+            .map {
+                it.split("\n")
+                    .sumOf { itemCaloriesStr -> itemCaloriesStr.trim().toInt() }
+            }
+
+    fun part1(input: String): Int {
+        return getListOfSummedElfCalories(input).max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        return getListOfSummedElfCalories(input).sortedDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24_000)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,10 +2,14 @@ import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
+fun readInput(name: String) = File("src", "$name.txt").readText()
+
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt").readLines()
+fun readInputLines(name: String) = File("src", "$name.txt").readLines()
+
+fun linesAsInts(lines: List<String>) = lines.map { l -> l.toInt() }
 
 /**
  * Converts string to md5 hash.
