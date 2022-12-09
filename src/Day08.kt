bff3f3ffebffa06060649e17a420341d176b4424
Michal Chomo <chomo.michal.sk@gmail.com>
2022-12-09 22:15:21 +0100

    add day 8

---
+++ src/Day08.kt
@@ -0,0 +1,44 @@
+fun main() {
+
+    fun part1(input: List<String>): Int = input.map { it.map { it.digitToInt() } }
+        .let { rows ->
+            rows.mapIndexed { rowIndex, cols ->
+                cols.mapIndexed cols@{ colIndex, height ->
+                    if (rowIndex !in 1 until rows.size - 1 || colIndex !in 1 until cols.size - 1) return@cols 0
+                    listOf(
+                        (cols.slice(0 until colIndex).all { it < height }),
+                        (cols.slice(colIndex + 1 until cols.size).all { it < height }),
+                        ((0 until rowIndex).all { rows[it][colIndex] < height }),
+                        ((rowIndex + 1 until rows.size).all { rows[it][colIndex] < height })
+                    ).any { it }.toInt()
+                }.sum()
+            }.sum()
+        }.plus(input.size * 4 - 4)
+
+    fun part2(input: List<String>): Int = input.map { it.map { it.digitToInt() } }
+        .let { rows ->
+            rows.mapIndexed { rowIndex, cols ->
+                cols.mapIndexed cols@{ colIndex, height ->
+                    if (rowIndex !in 1 until rows.size - 1 || colIndex !in 1 until cols.size - 1) return@cols 0
+                    listOf(
+                        (colIndex - 1 downTo 0).filter { cols[it] >= height }
+                            .maxOrNull()?.let { colIndex - it } ?: colIndex,
+                        (colIndex + 1 until cols.size).filter { cols[it] >= height }
+                            .minOrNull()?.let { it - colIndex } ?: (cols.size - 1 - colIndex),
+                        (rowIndex - 1 downTo 0).filter { rows[it][colIndex] >= height }
+                            .maxOrNull()?.let { rowIndex - it } ?: rowIndex,
+                        (rowIndex + 1 until rows.size).filter { rows[it][colIndex] >= height }
+                            .minOrNull()?.let { it - rowIndex } ?: (rows.size - 1 - rowIndex),
+                    ).reduce { a, b -> a * b }
+                }.max()
+            }.max()
+        }
+
+    val testInput = readInputLines("Day08_test")
+    check(part1(testInput) == 21)
+    check(part2(testInput) == 8)
+
+    val input = readInputLines("Day08")
+    println(part1(input))
+    println(part2(input))
+}
