f31bdb56414090c52437f11685e8dfbede6a65d7
Michal Chomo <michal.chomo@proton.me>
2023-12-29 13:12:51 +0100

    [fix] day 12 part 2

--- src/main/kotlin/eu/michalchomo/adventofcode/year2023/Day12.kt
+++ src/main/kotlin/eu/michalchomo/adventofcode/year2023/Day12.kt
@@ -2,74 +2,69 @@ package eu.michalchomo.adventofcode.year2023
 
 import eu.michalchomo.adventofcode.Day
 import eu.michalchomo.adventofcode.main
+import eu.michalchomo.adventofcode.println
 
 object Day12 : Day {
 
     override val number: Int = 12
 
-    override fun part1(input: List<String>): String = input.sumOf { processLine(it) }.toString()
+    override fun part1(input: List<String>): String = solve(input, unfoldTimes = 1)
 
-    override fun part2(input: List<String>): String = input.sumOf { line ->
-        val a = processLine(line)
-        val a2 = processLine(line.unfold(2))
-        val a3 = a2 / a * a2
-        val a4 = a3 / a2 * a3
-        a4 / a3 * a4
+    override fun part2(input: List<String>): String = solve(input, unfoldTimes = 5)
+
+    private fun solve(input: List<String>, unfoldTimes: Int = 1): String = input.sumOf { line ->
+        cache.clear()
+        line.unfold(unfoldTimes).split(' ').let {
+            processLineRecursive(it[0], it[1].digitsToIntList(), 0, 0, 0)
+        }
     }.toString()
 
-    private fun processLine(line: String): Int {
-        val damagedGroupsSizes = line.split(' ')[1].split(',')
-            .map { it.toInt() }
-        return line.split(' ')[0].variants(damagedGroupsSizes)
-            .map { ".${it.joinToString("")}." }
-            .count {
-                damagedGroupsSizes.fold(it) { acc, damagedCount ->
-                    val damagedGroup = ".${"#".repeat(damagedCount)}."
-                    val firstDamaged = acc.indexOf('#')
-                    val i = acc.indexOf(damagedGroup)
-                    if (i == -1 || i >= firstDamaged) {
-                        return@count false
-                    }
-                    val newStartIndex = i + damagedGroup.length - 1
-                    if (newStartIndex < acc.length) acc.substring(newStartIndex) else ""
-                }
-                true
-            }
-    }
-
-    private fun CharArray.countSymbol(symbol: Char): Int = this.count { it == symbol }
-
-    private fun String.variants(damagedGroupsSizes: List<Int>): Set<CharArray> {
-        val indices = this.withIndex().filter { it.value == '?' }.map { it.index }
-        val alphabet = charArrayOf('.', '#')
-        val variants = mutableSetOf<CharArray>()
-        val maxGroupSize = damagedGroupsSizes.max()
-        val damagedSpringsCount = damagedGroupsSizes.sum()
-        loop@ for (i in 0..<(1L shl indices.size)) {
-            val variant = this.toCharArray()
-            for (j in indices.indices) {
-                if (variant.countSymbol('#') > damagedSpringsCount
-                    || variant.containsContiguousGroupOfSize(maxGroupSize+1)) {
-                    continue@loop
-                }
-                val addedSymbol = alphabet[((i shr j) and 1).toInt()]
-                variant[indices[j]] = addedSymbol
-            }
-            if (variant.countSymbol('#') != damagedSpringsCount) continue@loop
-            variants.add(variant)
+    private val cache = mutableMapOf<Triple<Int, Int, Int>, Long>()
+    private fun processLineRecursive(
+        springs: String,
+        damagedGroupsSizes: List<Int>,
+        springIndex: Int,
+        groupIndex: Int,
+        currentGroupSize: Int
+    ): Long {
+        val key = Triple(springIndex, groupIndex, currentGroupSize)
+        if (key in cache) {
+            return cache[Triple(springIndex, groupIndex, currentGroupSize)]!!
         }
-        return variants
+        if (springIndex == springs.length) {
+            if (groupIndex == damagedGroupsSizes.size && currentGroupSize == 0) return 1
+            if (groupIndex == damagedGroupsSizes.size - 1 && damagedGroupsSizes[groupIndex] == currentGroupSize) return 1
+            return 0
+        }
+        var arrangements = 0L
+        for (c in charArrayOf('.', '#')) {
+            if (springs[springIndex] == c || springs[springIndex] == '?') {
+                if (c == '.' && currentGroupSize == 0) {
+                    arrangements += processLineRecursive(springs, damagedGroupsSizes, springIndex + 1, groupIndex, 0)
+                } else if (c == '.' && currentGroupSize > 0 && groupIndex < damagedGroupsSizes.size && currentGroupSize == damagedGroupsSizes[groupIndex]) {
+                    arrangements += processLineRecursive(
+                        springs,
+                        damagedGroupsSizes,
+                        springIndex + 1,
+                        groupIndex + 1,
+                        0
+                    )
+                } else if (c == '#') {
+                    arrangements += processLineRecursive(
+                        springs,
+                        damagedGroupsSizes,
+                        springIndex + 1,
+                        groupIndex,
+                        currentGroupSize + 1
+                    )
+                }
+            }
+        }
+        cache[key] = arrangements
+        return arrangements
     }
 
-    private fun CharArray.containsContiguousGroupOfSize(groupSize: Int): Boolean {
-        this.fold(0) { acc, c ->
-            if (acc == groupSize) return true
-            if (c == '#') {
-                 acc + 1
-            } else 0
-        }
-        return false
-    }
+    private fun String.digitsToIntList(): List<Int> = this.split(',').map { it.toInt() }
 
     private fun String.unfold(times: Int): String {
         val springsAndGroupSizes = this.split(' ')
